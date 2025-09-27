98ac5a69b3dc5f497974e64fec469d9e25ce7726
Ian Snider <iqsnider@gmail.com>
2025-09-26 23:22:02 -0700

    feat: window event polling

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main() {
 	std::cout << loc.y << std::endl;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("Circle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Circle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,  SDL_WINDOW_ALWAYS_ON_TOP);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	bool running = true;
