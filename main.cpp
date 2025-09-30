c8cc73d8f4c346a7eae0cd01ff8da57fa52972ec
Ian Snider <iqsnider@gmail.com>
2025-09-29 23:03:43 -0700

    impr: smoothing

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   SDL_Event event;
 
-  RandomWalk new_walk(200, 20, 100);
+  RandomWalk new_walk(200, 20, 50);
   new_walk.render_self_avoiding_walk(event, window, renderer);
 
   SDL_DestroyRenderer(renderer);
